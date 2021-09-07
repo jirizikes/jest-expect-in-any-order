ca5ef0f51af3043cd72bfa2ba02611e3e49c4ad4
jirizikes <zikes@collabim.com>
2021-09-07 02:24:17 +0200

    jest-expect-in-any-order

---
+++ src/functions/expectInAnyOrder.test.ts
@@ -0,0 +1,116 @@
+import { expectInAnyOrder } from "./expectInAnyOrder";
+
+test("Two same objects ", () => {
+  expect(
+    expectInAnyOrder(
+      {
+        string: "hi",
+        array: [1, 2, 3],
+      },
+      {
+        string: "hi",
+        array: [1, 2, 3],
+      }
+    )
+  ).toBeUndefined();
+});
+
+test("Two different objects ", () => {
+  expect(() =>
+    expectInAnyOrder(
+      {
+        string: "hi",
+        array: [1, 2, 3],
+      },
+      {
+        string: "hello",
+        array: [1, 3, 2],
+      }
+    )
+  ).toThrow("Not same");
+});
+
+test("Two different objects 2", () => {
+  expect(() =>
+    expectInAnyOrder(
+      {
+        string: "hi",
+        array: [
+          {
+            name: "CZ",
+          },
+          {
+            name: "EN",
+          },
+          {
+            name: "DE",
+          },
+        ],
+      },
+      {
+        string: "hi",
+        array: [
+          {
+            name: "EN",
+          },
+          {
+            name: "CZZ",
+          },
+          {
+            name: "DE",
+          },
+        ],
+      }
+    )
+  ).toThrow("Not same");
+});
+
+test("Two same objects, different order", () => {
+  expect(
+    expectInAnyOrder(
+      {
+        string: "hi",
+        array: [1, 2, 3],
+      },
+      {
+        string: "hi",
+        array: [1, 3, 2],
+      }
+    )
+  ).toBeUndefined();
+});
+
+test("Two same objects, different order 2", () => {
+  expect(
+    expectInAnyOrder(
+      {
+        string: "hi",
+        array: [
+          {
+            name: "CZ",
+          },
+          {
+            name: "EN",
+          },
+          {
+            name: "DE",
+          },
+        ],
+      },
+      {
+        string: "hi",
+        array: [
+          {
+            name: "EN",
+          },
+          {
+            name: "CZ",
+          },
+          {
+            name: "DE",
+          },
+        ],
+      }
+    )
+  ).toBeUndefined();
+});
